32f5c3b272d61bad08b4dc5c27bf35be1b197e4d
Sanskriti Lamsal <sanskriti.lamsal@slalom.com>
2020-08-11 09:24:31 -0400

    working conversation

---
+++ lambda/util.js
@@ -0,0 +1,36 @@
+const AWS = require('aws-sdk');
+const Alexa = require('ask-sdk-core');
+
+const s3SigV4Client = new AWS.S3({
+    signatureVersion: 'v4',
+    region: process.env.S3_PERSISTENCE_REGION
+});
+
+/**
+ * Helper method to find if a request is for a certain apiName.
+ */
+const isApiRequest = (handlerInput, apiName) => {
+    try {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'Dialog.API.Invoked'
+            && handlerInput.requestEnvelope.request.apiRequest.name === apiName;
+    } catch (e) {
+        console.log('Error occurred: ', e);
+        return false;
+    }
+}
+module.exports = {
+    isApiRequest: isApiRequest
+};
+
+module.exports.getS3PreSignedUrl = function getS3PreSignedUrl(s3ObjectKey) {
+
+    const bucketName = process.env.S3_PERSISTENCE_BUCKET;
+    const s3PreSignedUrl = s3SigV4Client.getSignedUrl('getObject', {
+        Bucket: bucketName,
+        Key: s3ObjectKey,
+        Expires: 60*1 // the Expires is capped for 1 minute
+    });
+    console.log(`Util.s3PreSignedUrl: ${s3ObjectKey} URL ${s3PreSignedUrl}`);
+    return s3PreSignedUrl;
+
+}
